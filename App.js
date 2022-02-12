4f40c5a0cc91eb3895abdc23c13378ac14cd24c8
Solomon Amsalu <solamsalu@gmail.com>
2022-02-12 08:23:35 +0300

    app.js is modified

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is deployment demo.
+          Success.
         </p>
         
       </header>
